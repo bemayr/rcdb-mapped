b5c5f97fad51684b42778daa410c420b49525f2b
Bernhard Mayr <bernhard@mayr.io>
2018-11-03 14:59:30 +0100

    integrated coaster_parser.py into scrape.py

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import requests
+import json
 from bs4 import BeautifulSoup
 
 from config import urls, gist
@@ -14,7 +15,6 @@ def get_max_page_number() -> int:
             in BeautifulSoup(requests.get(urls["coasters"]["index"]).content, "html.parser").find(id="rfoot").find_all("a")
             if pagelink.get_text().isnumeric()]))
 
-
 def get_coaster_ids(max_page_number) -> [int]:
     return [entry.find_all("a")[1].get("href")[1:].split(".")[0]
             for page_number
@@ -22,29 +22,23 @@ def get_coaster_ids(max_page_number) -> [int]:
             for entry
             in BeautifulSoup(requests.get(urls["coasters"]["page"] + str(page_number)).content, "html.parser").select("#report tbody")[0].find_all("tr")]
 
-
 def read_gist_token() -> str:
     with open('GIST_TOKEN', 'r') as token_file:
         return token_file.read().rstrip()
-
 
 def update_gist(coasters):
     return requests.patch(f'{gist["url"]}{gist["id"]}', headers={"Authorization": f'token {read_gist_token()}'},
                           data=gist["template"] % coasters)
 
-
 def create_gist_response_message(response) -> str:
     return f'Yay, the coasters have been updated successfully! ({response.url})' if response.ok else f'Oh noooo, something unexpected happened: {response.status_code}, {response.reason}'
-
 
 def main():
     print("Counting all the coasters...")
     max_page_number = get_max_page_number()
     coaster_ids = get_coaster_ids(max_page_number)
     print("Riding all the coasters to get the necessary information...")
-    coasters = extract_coasters(coaster_ids)  # replace with David's code
-    # coasters to JSON via `json.dumps(coasters, cls=CoasterEncoder)`
-    # or via `print(coaster)`
+    coasters = json.dumps(extract_coasters(coaster_ids), cls=CoasterEncoder)
     print("Storing the data in the cloud")
     response = update_gist(coasters)
     print(create_gist_response_message(response))
